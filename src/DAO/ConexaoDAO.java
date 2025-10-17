7b3a5752d4d00af13caf7f7eaeb87b6ae1a5de71
AnClockup <varjao2003@yahoo.com.br>
2025-10-17 04:47:51 -0300

    Autenticação de Usuário - Login JAVA com MYSQL

---
+++ src/DAO/ConexaoDAO.java
@@ -0,0 +1,26 @@
+package DAO;
+
+
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoDAO {
+    
+    public Connection conectaBD(){
+        
+        Connection conn = null;
+        
+        try {
+            String url = "jdbc:mysql://localhost:3306/bancoteste?user=root&password=";
+            
+            conn = DriverManager.getConnection(url);
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "ConexaoDAO "+ erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
